28c0f23bc7ef7ab9a19f73aca574bf79219ac799
katherin <katherin.garciaVA1@gmail.com>
2023-05-11 15:06:43 -0400

    updating a notebook route done

--- backend/routes/api/notebooks.js
+++ backend/routes/api/notebooks.js
@@ -4,6 +4,7 @@ const { Op } = require('sequelize');
 const { Notebook } = require('../../db/models');
 const { requireAuth } = require("../../utils/auth");
 
+
 //get all notebooks
 router.get('/', async (req, res) => {
     const allNotebooks = await Notebook.findAll();
@@ -21,6 +22,28 @@ router.post('/', requireAuth, async (req, res) => {
     return res.json({
         user
     })
+})
+
+//edit an exisiting notebook
+router.put('/:notebookId', requireAuth, async (req, res) => {
+    const {title} = req.body;
+    const notebookId = req.params.notebookId
+
+    const notebook = await Notebook.findByPk(notebookId)
+
+    //checks to see if the tag does not exists
+    if(!notebook) return res.status(404).json({message: "Notebook not found", statusCode: 404})
+
+    //checks to see if the current user is the owner of the tag
+    if(notebook.userId !== req.user.id) return res.status(403).json({message: "Not Authorized", statusCode: 403})
+
+    //update the Tag with the new data
+    const updateNotebook = await notebook.update({
+       title
+    })
+
+    return res.status(200).json(updateNotebook)
+
 })
 
 
