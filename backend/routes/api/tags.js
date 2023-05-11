d3496ae09dbfd357a97d8369f61448dec34e0bf2
katherin <katherin.garciaVA1@gmail.com>
2023-05-11 14:32:56 -0400

    deleting tag route done

--- backend/routes/api/tags.js
+++ backend/routes/api/tags.js
@@ -49,6 +49,34 @@ router.put('/:tagId', requireAuth, async (req, res) => {
 
 })
 
+//deleting a tag
+router.delete('/:tagId', requireAuth, async (req, res) => {
+    const tagId = req.params.tagId
+    const userId = req.user.id
+
+    const tag = await Tag.findByPk(tagId);
+
+    //check to see if the tag exists
+    if(!tag){
+        return res.status(404).json({message: "Tag couldn't be found", statusCode: 403})
+    }
+    // check to see if the user id matches the tag user id
+    if(tag.userId !== userId){
+        return res.status(403).json({
+            message: "Not Authorized",
+            statusCode: 403
+        })
+    }
+
+    await tag.destroy();
+
+    return res.status(200).json({
+        message: "Successfully deleted",
+        statusCode: 200
+    })
+
+}) 
+
 
 
 
