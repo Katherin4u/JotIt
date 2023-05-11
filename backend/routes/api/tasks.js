4e432bcb45a5f612f7322553860e4debfc208fae
jose <antonysoly2217@gmail.com>
2023-05-11 19:52:33 -0400

    created edit, delete and fixed post task route

--- backend/routes/api/tasks.js
+++ backend/routes/api/tasks.js
@@ -16,9 +16,9 @@ router.post(
         const { text, priority } = req.body;
         const task = await Task.create({ userId, text, priority })
 
-        // return res.json(
-        //     task
-        // )
+        return res.json(
+            task
+        )
     }
 );
 
@@ -51,5 +51,55 @@ router.get(
         return res.json(task)
     }
 );
+
+// edit an exisiting task
+router.put('/:taskId', requireAuth, async (req, res) => {
+    const { text, priority, } = req.body;
+    const taskId = req.params.taskId
+
+    const task = await Task.findByPk(taskId)
+
+    //checks to see if the task does not exists
+    if (!task) return res.status(404).json({ message: "task not found", statusCode: 404 })
+
+
+    //update the task with the new data
+    const updateTask = await task.update({
+        text,
+        priority
+    })
+
+    return res.status(200).json(updateTask)
+
+})
+
+
+//deleting a task
+router.delete('/:taskId', requireAuth, async (req, res) => {
+    const taskId = req.params.taskId
+    const userId = req.user.id
+
+    const task = await Task.findByPk(taskId);
+
+    //check to see if the task exists
+    if (!task) {
+        return res.status(404).json({ message: "task couldn't be found", statusCode: 403 })
+    }
+    // check to see if the user id matches the task user id
+    if (task.userId !== userId) {
+        return res.status(403).json({
+            message: "Not Authorized",
+            statusCode: 403
+        })
+    }
+
+    await task.destroy();
+
+    return res.status(200).json({
+        message: "Successfully deleted",
+        statusCode: 200
+    })
+
+})
 
 module.exports = router;
